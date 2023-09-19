d65ec4c7a400c86c0d7e09d0bb635c2d8170ec2b
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-09-19 01:02:29 -0300

    Add files via upload

---
+++ 04_marker_map_IconoPersonalizado.py
@@ -0,0 +1,19 @@
+import folium
+
+# Crea un objeto de mapa centrado en una ubicación específica
+londres = folium.Map(location=[51.5074, -0.1278], zoom_start=10)  # Latitud y longitud de Londres
+
+# Agrega un marcador al mapa
+folium.Marker(
+    location=[51.5074, -0.1278],  # Latitud y longitud del marcador
+    popup='Londres',  # Texto emergente del marcador
+    icon=folium.Icon(color="green", icon="leaf", icon_color="white", prefix="glyphicon")  # Ícono del marcador personalizado
+
+).add_to(londres)
+
+# Guarda el mapa como un archivo HTML
+londres.save('mapa_londres.html')
+
+# Para verlo desde la consola de Python
+import os
+os.system('start mapa_londres.html')
